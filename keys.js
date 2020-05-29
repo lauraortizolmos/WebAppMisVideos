f2aa537863dd1a17d090b0e76c025c13142458e6
lauraortizolmos <laura.ortizolmos@gmail.com>
2020-05-29 03:48:57 -0500

    Web app

---
+++ keys.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'sql3.freemysqlhosting.net',
+        user: 'sql3343600',
+        password: '5nunyn4W2K',
+        database: 'sql3343600'
+      
+
+    }
+
+};
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const videosController = require('../controllers/index');
+
+
+router.get('/', videosController.listIndex);
+router.get('/api', videosController.list);
+router.get('/detail/:id', videosController.detail);
+
+router.post('/detail/:id', videosController.detail);
+
+router.get('/api/detail/:id', videosController.details);
+router.post('/api/detail/:id', videosController.details);
+
+router.post('/results', videosController.results);
+router.get('/results', (req, res) => {
+    res.render('results');
+});
+
+router.get('/contact', (req, res) => {
+    res.render('contact');
+});
+
+
+
+
+module.exports = router;
